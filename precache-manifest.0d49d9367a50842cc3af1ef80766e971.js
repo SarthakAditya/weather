83c96f70ba43e6b2b40d41bd359797b5bc7274db
Sarthak Aditya <sarthak@Sarthaks-MacBook-Pro.local>
2019-04-01 21:47:51 +0530

    Updates

---
+++ precache-manifest.0d49d9367a50842cc3af1ef80766e971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81c33a0ca453b84c010a",
+    "url": "/weather/static/css/main.f9ae1c59.chunk.css"
+  },
+  {
+    "revision": "81c33a0ca453b84c010a",
+    "url": "/weather/static/js/main.6352a2e6.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "c288e8b9b0279120e6eb",
+    "url": "/weather/static/css/2.ca98e80b.chunk.css"
+  },
+  {
+    "revision": "c288e8b9b0279120e6eb",
+    "url": "/weather/static/js/2.b9051853.chunk.js"
+  },
+  {
+    "revision": "05cd77a2f2033032414327ec5f20e862",
+    "url": "/weather/index.html"
+  }
+];
