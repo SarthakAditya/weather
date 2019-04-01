67828900b4df5e7814d2c0b9b20fe6cf7e5d8894
Sarthak Aditya <sarthak@Sarthaks-MacBook-Pro.local>
2019-04-01 12:01:48 +0530

    basic weather app working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Titles from './components/Titles'
+import From from  './components/Form'
+import Wearher from './components/Weather'
+
+const API_KEY="8471d14a7f834e1595840614190104";
 
 class App extends Component {
+  state={
+    temp:undefined,
+    city:undefined,
+    country:undefined,
+    prec:undefined,
+    humidity:undefined,
+    error:undefined
+
+  };
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city=e.target.elements.city.value;
+    const country=e.target.elements.country.value;
+    if (city && country)
+    {
+      const api_call= await fetch(`http://api.apixu.com/v1/current.json?key=${API_KEY}&q=${city}`);
+      const data = await api_call.json();
+      console.log(data);
+      this.setState({
+        temp: data.current.temp_c,
+        city: data.location.name,
+        country:data.location.country,
+        prec:data.current.precip_mm,
+        humidity:data.current.humidity,
+        error: ""
+      })
+    }
+    else
+    {
+      this.setState({error: "Please Enter Correct Details"})
+    }
+
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+          <Titles/>
+          <From getWeather={this.getWeather}/>
+          <Wearher
+            temperature={this.state.temp}
+            city={this.state.city}
+            country={this.state.country}
+            precipitation={this.state.prec}
+            humidity={this.state.humidity}
+            error={this.state.error}
+          />
+        </div>
     );
   }
+
 }
 
 export default App;
